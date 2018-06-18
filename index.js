8021d39931e038f56cfdfeff49d07b3b888d2485
Emmanuel D Mendez <manche65@gmail.com>
2018-06-18 10:04:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ function logWhisper(string){
 }
 
 function sayHiToGrandma(string){
+<<<<<<< HEAD
   if(string.toUpperCase() === string){
     return 'YES INDEED!'
   }
@@ -24,4 +25,21 @@ function sayHiToGrandma(string){
   if(string === "I love you, Grandma."){
     return 'I love you, too.'
   }
+=======
+  // var uppercase = 'HELLO';
+  // if(uppercase.toUpperCase() === uppercase){
+  //   return 'YES INDEED!'
+  // }
+  var lowercase = 'hello'
+  if(lowercase.toLowerCase() === lowercase){
+    return 'I can\'t hear you!'
+  }
+  // var mixedCase = 'Hi there';
+  // if(mixedCase.toLowerCase() === mixedCase){
+  //   return 'I love you, too.'
+  // }
+  // if(mixedCase.toUpperCase() === mixedCase){
+  //   return ''
+  // }
+>>>>>>> 6a33a657ae56ebf8987c5a2e77eca10f76c6105e
 }
